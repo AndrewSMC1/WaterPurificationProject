ffa88cabbc3f1316f4d856ea41a1da6d2025391b
kylemensinger <106849650+kylemensinger@users.noreply.github.com>
2023-05-12 15:31:16 -0700

    Update math mod final project.py

--- math mod final project.py
+++ math mod final project.py
@@ -10,7 +10,9 @@ Created on Thu May 11 18:54:52 2023
 # =============================================================================
 
 
-mass = 8.34
+mass = 8.34 #this is the weight of water per gallon
+#this weight per gal will give us the a similar result since waste is mostly water
+
 days = 28
 
 #28 days for all to get each tank below 1%
